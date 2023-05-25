425c213bbe9fbb755740bab0f987d35829c67e2c
Dadoum <24679280+Dadoum@users.noreply.github.com>
2023-05-25 22:22:00 +0200

    Update FAT.swift

--- Sources/SwiftMachO/MachO/FAT/FAT.swift
+++ Sources/SwiftMachO/MachO/FAT/FAT.swift
@@ -8,6 +8,16 @@
 
 import Foundation
 
+public let CPU_TYPE_ANY: UInt32 = UInt32.max
+public let CPU_ARCH_ABI64: UInt32 = 0x1000000
+public let CPU_TYPE_X86: UInt32 = 7
+public let CPU_TYPE_I386: UInt32 = CPU_TYPE_X86   // For compatibility
+public let CPU_TYPE_X86_64: UInt32 = CPU_TYPE_X86 | CPU_ARCH_ABI64
+public let CPU_TYPE_ARM: UInt32 = 12
+public let CPU_TYPE_ARM64: UInt32 = CPU_TYPE_ARM | CPU_ARCH_ABI64
+public let CPU_TYPE_POWERPC: UInt32 = 18
+public let CPU_TYPE_POWERPC64: UInt32 = CPU_TYPE_POWERPC | CPU_ARCH_ABI64
+
 public enum FATError: Error {
     case notFAT
     case noBestArchFound
